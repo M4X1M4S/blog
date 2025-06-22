4c8ce6c9b1f4ed8aa6d74b4b587baeee1bfae65b
ABHILASH <99235816+M4X1M4S@users.noreply.github.com>
2025-06-22 17:23:27 +0530

    cleaer

--- services/user/src/server.ts
+++ services/user/src/server.ts
@@ -8,10 +8,7 @@ connectToDatabase();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/v1", userRoutes);
-app.get("/", (req: Request, res: Response) => {
-  console.log("API is running");
-  res.send("API is running");
-});
+
 app.listen(PORT, () => {
   console.log(`User service is running on port ${PORT}`);
 });
